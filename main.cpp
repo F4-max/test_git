21eeb8f686c52961496ab0258004d87904fe54f3
famax <maximefmc9@gmail.com>
2026-03-14 17:53:19 +0100

    correction of main.cpp and add of addition function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 
+int somme(const int a, const int b){
+
+return a + b;
+
+}
+
 int main(){
 
-std::cout << "Hello Main ! << std::endl;
+int a = 3;
+int b = 9;
+
+std::cout << "Hello Main !\n";
+std::cout << "a = " << a << " et b = " << b;
+
+std::cout << "\na + b = " << a << " + " << b << " = " << somme(a, b) << std::endl;
 
 }
