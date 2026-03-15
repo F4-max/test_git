650bc48fbc105abb8526a697cd0059d76ff281c2
maxime <maxime.fauremc@gmail.com>
2026-03-16 00:32:54 +0100

    add of division operation

--- main.cpp
+++ main.cpp
@@ -12,6 +12,13 @@ return a*b;
 
 }
 
+int division(const int a, const int b){
+
+if (b == 0)
+	return 0;
+return a / b;
+}
+
 int main(){
 
 int a = 3;
@@ -23,6 +30,8 @@ std::cout << "a = " << a << " et b = " << b;
 std::cout << "\na + b = " << a << " + " << b << " = " << somme(a, b);
 
 std::cout << "\na X b = " << a << " X " << b << " = " << produit(a, b);
+
+std::cout << "\na / b = " << a << " / " << b << " = " << division(a, b);
 
 std::cout << "\nC'est tout !" << std::endl;
 
